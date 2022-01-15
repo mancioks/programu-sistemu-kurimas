d70bb3d7a8973f533fa2f35e3e0a2f6e33324900
mancioks <mantas.kryzevicius@gmail.com>
2022-01-15 17:38:19 +0200

    pages

--- pages/404.php
+++ pages/404.php
@@ -1,10 +1,1 @@
-<div class="card">
-    <div class="card-content">
-        <p class="title">
-            Klaida
-        </p>
-        <p class="subtitle">
-            Puslapis nerastas
-        </p>
-    </div>
-</div>
+Klaida
